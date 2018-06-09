0933ba2fcc4ed1a54b3051f6be64c1f1ed3ae3e1
viaviare <viaviare@yandex.ru>
2018-06-09 23:44:07 +0300

    add logging with logback

--- addressbook/src/test/java/ru/stqa/pft/addbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addbook/tests/TestBase.java
@@ -1,20 +1,38 @@
 package ru.stqa.pft.addbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app =
           new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
 
   @BeforeSuite
-  public void Start(){
+  public void Start() throws IOException {
     app.init();
   }
 
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] params){
+    logger.info("start " + m  + " parameters " + Arrays.asList(params));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("stop " + m);
+  }
 }
 
