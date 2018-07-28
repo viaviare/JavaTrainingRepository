98850047260e62ee84e434cb6b6b84ada597e7d7
viaviare <viaviare@yandex.ru>
2018-07-29 01:58:09 +0300

    add optional checks comparing UI with DB

--- addressbook/src/test/java/ru/stqa/pft/addbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addbook/tests/TestBase.java
@@ -7,10 +7,17 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addbook.model.ContSet;
+import ru.stqa.pft.addbook.model.GroupData;
+import ru.stqa.pft.addbook.model.GroupSet;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 public class TestBase {
 
@@ -33,6 +40,25 @@ public class TestBase {
   @AfterMethod (alwaysRun = true)
   public void logTestStop(Method m){
     logger.info("stop " + m);
+  }
+
+
+  public void verifyGroupListInUI() {
+    if(Boolean.getBoolean("verifyUI")) {
+      GroupSet dbGroups = app.getDbH().groups();
+      GroupSet uiGroups = app.getGroupH().getGroupSet();
+      assertThat(uiGroups, equalTo(dbGroups.stream().map((g) ->
+              new GroupData().setId(g.getId()).setName(g.getName()))
+              .collect(Collectors.toSet())));
+    }
+  }
+
+  public void verifyContactListInUI() {
+    if(Boolean.getBoolean("verifyUI")) {
+      ContSet dbContacts = app.getDbH().contacts();
+      ContSet uiContacts = app.getContactH().getContactSetList();
+      assertThat(uiContacts, equalTo(dbContacts));
+    }
   }
 }
 
